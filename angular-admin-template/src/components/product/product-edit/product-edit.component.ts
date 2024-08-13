ae2f2f1fc6ee8065c6fc60cfc8fbedce5c4a421d
Hassan Iqbal <hassaniqbal.dev@gmail.com>
2024-08-13 15:51:09 +0500

    update product done

--- angular-admin-template/src/components/product/product-edit/product-edit.component.ts
+++ angular-admin-template/src/components/product/product-edit/product-edit.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-edit',
+  selector: 'app-product-add',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-edit.component.html',
-  styleUrl: './product-edit.component.scss'
+  styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent {
+  product = {id:0, name: '', price: 0 };
 
+  constructor(private productService: ProductService, private router: Router) {}
+
+  onSubmit() {
+    this.productService.updateProduct(this.product.id, {
+      id: this.product.id,
+      name: this.product.name,
+      price: this.product.price
+    });
+    this.router.navigate(['/product']);
+  }
 }
